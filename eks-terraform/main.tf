688e92cbccc7417c5fa02315aef574a840ed8dfd
shashanka2911 <shashanka2911@gmail.com>
2026-09-08 21:09:59 +0530

    Fix S3 backend bucket and EKS node size

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 # ----------------------------
 # IAM Role for EKS Cluster
 # ----------------------------
-resource "aws_iam_role" "master" {
-  name = "yaswanth-eks-master1"
+resource "aws_iam_role" "master1" {
+  name = "shashanka-eks-master1"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -39,7 +39,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 # IAM Role for Worker Nodes
 # ----------------------------
 resource "aws_iam_role" "worker" {
-  name = "yaswanth-eks-worker1"
+  name = "shashanka-eks-worker1"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -54,7 +54,7 @@ resource "aws_iam_role" "worker" {
 }
 
 resource "aws_iam_policy" "autoscaler" {
-  name = "yaswanth-eks-autoscaler-policy1"
+  name = "shashanka-eks-autoscaler-policy1"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -171,6 +171,9 @@ resource "aws_eks_cluster" "eks" {
 # ----------------------------
 # EKS Node Group
 # ----------------------------
+# ----------------------------
+# EKS Node Group
+# ----------------------------
 resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = var.node_group_name
@@ -178,7 +181,7 @@ resource "aws_eks_node_group" "node-grp" {
   subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
   capacity_type   = "ON_DEMAND"
   disk_size       = 20
-  instance_types  = ["t2.large"]
+  instance_types  = ["t3.small"] # <-- CHANGED THIS LINE
 
   labels = {
     env = "dev"
@@ -206,7 +209,6 @@ resource "aws_eks_node_group" "node-grp" {
     aws_iam_role_policy_attachment.autoscaler,
   ]
 }
-
 # ----------------------------
 # OIDC Provider for ServiceAccount IAM Roles
 # ----------------------------
